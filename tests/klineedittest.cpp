f4aa69937a11c0fe4aa6255faf9774cfeb029546
Aurélien Gâteau <agateau@kde.org>
2014-03-31 14:34:17 +0200

    Fix klineedittest segfault

--- tests/klineedittest.cpp
+++ tests/klineedittest.cpp
@@ -78,7 +78,6 @@ KLineEditTest::KLineEditTest(QWidget *widget)
     buttonLayout->addWidget(m_btnPassword);
     buttonLayout->addWidget(m_btnEnable);
     buttonLayout->addWidget(m_btnHide);
-    buttonLayout->addWidget(m_btnClickMessage);
     buttonLayout->addWidget(m_btnPlaceholderText);
     buttonLayout->addWidget(btnStyle);
 
