f22705635002783181d4f4cf6f4044f02fdedae3
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-06-07 11:23:39 +0200

    Convert license statements to SPDX expressions

--- src/klineedit.cpp
+++ src/klineedit.cpp
@@ -1,28 +1,15 @@
-/* This file is part of the KDE libraries
+/*
+    This file is part of the KDE libraries
 
-   Copyright (C) 1997 Sven Radej (sven.radej@iname.com)
-   Copyright (c) 1999 Patrick Ward <PAT_WARD@HP-USA-om5.om.hp.com>
-   Copyright (c) 1999 Preston Brown <pbrown@kde.org>
+    SPDX-FileCopyrightText: 1997 Sven Radej <sven.radej@iname.com>
+    SPDX-FileCopyrightText: 1999 Patrick Ward <PAT_WARD@HP-USA-om5.om.hp.com>
+    SPDX-FileCopyrightText: 1999 Preston Brown <pbrown@kde.org>
 
-   Re-designed for KDE 2.x by
-   Copyright (c) 2000, 2001 Dawit Alemayehu <adawit@kde.org>
-   Copyright (c) 2000, 2001 Carsten Pfeiffer <pfeiffer@kde.org>
+    Re-designed for KDE 2.x by
+    SPDX-FileCopyrightText: 2000, 2001 Dawit Alemayehu <adawit@kde.org>
+    SPDX-FileCopyrightText: 2000, 2001 Carsten Pfeiffer <pfeiffer@kde.org>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License (LGPL) as published by the Free Software Foundation;
-   either version 2 of the License, or (at your option) any later
-   version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include "klineedit.h"
--- src/klineedit.h
+++ src/klineedit.h
@@ -1,30 +1,17 @@
-/*  This file is part of the KDE libraries
+/*
+    This file is part of the KDE libraries
 
     This class was originally inspired by Torben Weis'
     fileentry.cpp for KFM II.
 
-    Copyright (C) 1997 Sven Radej <sven.radej@iname.com>
-    Copyright (c) 1999 Patrick Ward <PAT_WARD@HP-USA-om5.om.hp.com>
-    Copyright (c) 1999 Preston Brown <pbrown@kde.org>
+    SPDX-FileCopyrightText: 1997 Sven Radej <sven.radej@iname.com>
+    SPDX-FileCopyrightText: 1999 Patrick Ward <PAT_WARD@HP-USA-om5.om.hp.com>
+    SPDX-FileCopyrightText: 1999 Preston Brown <pbrown@kde.org>
 
     Completely re-designed:
-    Copyright (c) 2000,2001 Dawit Alemayehu <adawit@kde.org>
+    SPDX-FileCopyrightText: 2000, 2001 Dawit Alemayehu <adawit@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License (LGPL) as published by the Free Software Foundation;
-    either version 2 of the License, or (at your option) any later
-    version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #ifndef KLINEEDIT_H
